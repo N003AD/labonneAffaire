492b245fedbbed6cf8c3003f02c5a9e92c0f261c
cheikhtidiane diop <cheikhtidiane.diop2@unchk.edu.sn>
2024-12-27 00:39:26 +0100

    directive ngFor

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -8,26 +8,31 @@ import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
   styleUrl: './article.component.css'
 })
 export class ArticleComponent implements OnInit {
-  /* title: string = "Mon Article";
+/* title: string = "Mon Article";
   prixArticle: number = 150;
-  */
  ImageArticle: string = "https://via.placeholder.com/400x250" 
-@Input() titreArticle!: string;
-@Input() prixArticle?: number;
+  AltImg : string = "Mon image";
+  description: string = "Some quick example text to build on the card title and make up the bulk of the card's content."
+  dispo : boolean = true;
+ */
+@Input() nom!: string;
+@Input() prix?: number;
+@Input() description?: string;
+@Input() ImageArticle?:string;
+@Input() AltImg?: string;
+@Input() dispo?: boolean;  
+
 @Output() info = new EventEmitter<string>();
 
-  AltImg : string = "Mon image";
 
-  description: string = "Some quick example text to build on the card title and make up the bulk of the card's content."
   
   NbreLike: number = 0;
 
   comment: string = "Ceci est un Commentaire";
-  dispo : boolean = true;
   ngOnInit(): void {}
 
   OnLike(){
     this.NbreLike++;
-    this.info.emit(`. J'aime bien ce produit: ${this.titreArticle}`);
+    this.info.emit(`. J'aime bien ce produit: ${this.nom}`);
   }
 }
