e5c057f1ce6b88ffaf8cbaf78100cea373758d33
cheikhtidiane diop <cheikhtidiane.diop2@unchk.edu.sn>
2024-12-30 12:50:49 +0100

    ng Style & ngClass

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -15,6 +15,7 @@ export class ArticleComponent implements OnInit {
   description: string = "Some quick example text to build on the card title and make up the bulk of the card's content."
   dispo : boolean = true;
  */
+
 @Input() nom!: string;
 @Input() prix?: number;
 @Input() description?: string;
@@ -23,16 +24,28 @@ export class ArticleComponent implements OnInit {
 @Input() dispo?: boolean;  
 
 @Output() info = new EventEmitter<string>();
-
-
-  
   NbreLike: number = 0;
-
   comment: string = "Ceci est un Commentaire";
+jaime : boolean = true;
+
+  getColor(){
+    if(this.dispo===true){
+      return 'green';
+    } else {
+          return'red';
+    }
+  }
   ngOnInit(): void {}
 
   OnLike(){
-    this.NbreLike++;
+    // this.NbreLike++;
+    if(this.jaime=== true){
+      this.NbreLike++;
+      this.jaime =false;
+    }else{
+      this.NbreLike--;
+      this.jaime =true;
+    }
     this.info.emit(`. J'aime bien ce produit: ${this.nom}`);
   }
 }
