985e70d840f2ee821d82a4c90b7ceb02a0bc743e
cheikhtidiane diop <cheikhtidiane.diop2@unchk.edu.sn>
2024-12-30 16:56:54 +0100

    Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,69 +7,21 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SnapFace';
   message: string = " ";
-  listeArticle = [
-    { 
-      id: 1, 
-      nom: 'Produit 1', 
-      prix: 150 , 
-      description: "Un super vélo",
-      ImageArticle: "https://via.placeholder.com/400x250",  
-      AltImg: "Vélo",
-      dispo: true
-    },
-    {
-      id: 2,
-      nom: 'Produit 2',
-      prix: 250 ,
-      description: "Un super scooter",
-      ImageArticle: "https://via.placeholder.com/400x250",
-      AltImg: "Scooter",
-      dispo: false
-    },
-    {
-      id: 3,
-      nom: 'Produit 3',
-      prix: 300 ,
-      description: "Un super moto",
-      ImageArticle: "https://via.placeholder.com/400x250",
-      AltImg: "Moto",
-      dispo: false
-    },
-    {
-      id: 4,
-      nom: 'Produit 4',
-      prix: 400 ,
-      description: "Un super camion",
-      ImageArticle: "https://via.placeholder.com/400x250",
-      AltImg: "Camion",
-      dispo: true
-    },
-    {
-      id: 5,
-      nom: 'Produit 5',
-      prix: 500 ,
-      description: "Un super bateau",
-      ImageArticle: "https://via.placeholder.com/400x250",
-      AltImg: "Bateau",
-      dispo: false
-    },
-    {
-      id: 6,
-      nom: 'Produit 6',
-      prix: 600 ,
-      description: "Un super avion",
-      ImageArticle: "https://via.placeholder.com/400x250",
-      AltImg: "Avion",
-      dispo: true
-    }
-  ];
+  listeArticle: any = [];
 
+  constructor(private dataService: DataService){
+
+  }
+
+  ngOnInit(){
+    this.listeArticle = this.dataService.listeArticles;
+  }
   onAffiche(arg: string) {
     console.log('Vous avez cliqué sur le produit :'+ arg);
     return this.message = "Vous avez cliqué sur un produit" + arg;
-
   }
+
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  listeArticles = [
+    { 
+      id: 1, 
+      nom: 'Produit 1', 
+      prix: 150 , 
+      description: "Un super vélo",
+      ImageArticle: "https://via.placeholder.com/400x250",  
+      AltImg: "Vélo",
+      dispo: true
+    },
+    {
+      id: 2,
+      nom: 'Produit 2',
+      prix: 250 ,
+      description: "Un super scooter",
+      ImageArticle: "https://via.placeholder.com/400x250",
+      AltImg: "Scooter",
+      dispo: false
+    },
+    {
+      id: 3,
+      nom: 'Produit 3',
+      prix: 300 ,
+      description: "Un super moto",
+      ImageArticle: "https://via.placeholder.com/400x250",
+      AltImg: "Moto",
+      dispo: false
+    },
+    {
+      id: 4,
+      nom: 'Produit 4',
+      prix: 400 ,
+      description: "Un super camion",
+      ImageArticle: "https://via.placeholder.com/400x250",
+      AltImg: "Camion",
+      dispo: true
+    },
+    {
+      id: 5,
+      nom: 'Produit 5',
+      prix: 500 ,
+      description: "Un super bateau",
+      ImageArticle: "https://via.placeholder.com/400x250",
+      AltImg: "Bateau",
+      dispo: false
+    },
+    {
+      id: 6,
+      nom: 'Produit 6',
+      prix: 600 ,
+      description: "Un super avion",
+      ImageArticle: "https://via.placeholder.com/400x250",
+      AltImg: "Avion",
+      dispo: true
+    }
+  ];
+  constructor() { }
+}
