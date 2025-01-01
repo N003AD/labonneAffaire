2b50a26b698944264f6e8acc4ea195482685df2c
cheikhtidiane diop <cheikhtidiane.diop2@unchk.edu.sn>
2025-01-01 17:04:50 +0100

    Formulaire basé sur Template: ngModel

--- src/app/ajouter/ajouter.component.ts
+++ src/app/ajouter/ajouter.component.ts
@@ -8,30 +8,20 @@ import { NgForm } from '@angular/forms';
   styleUrl: './ajouter.component.css'
 })
 export class AjouterComponent implements OnInit {
-  TitleToAdd: string = '';
-  PriceToAdd: string = '';
-  message: string = '';
+TitleToAdd = "";
+PriceToAdd = "" ;
+message: string = '';
 
-  constructor() {}
+constructor(){}
+ngOnInit(): void {
+}
 
-  ngOnInit(): void {}
+onSubmit(form: NgForm):void{
+  const newTitle = this.TitleToAdd;
+  const newPrice = this.PriceToAdd;
+  console.log("New Title: " + newTitle);
+  console.log("New Price: " + newPrice);  
+  this.message = 'Veuillez remplir tous les champs.';
+}
 
-  onSubmit(form: NgForm): void {
-    if (form.valid) {
-      const newTitle = this.TitleToAdd;
-      const newPrice = this.PriceToAdd;
-      console.log(`New Title: ${newTitle}`);
-      console.log(`New Price: ${newPrice}`);
-
-      // Reset the form fields
-      this.TitleToAdd = '';
-      this.PriceToAdd = '';
-      form.resetForm();
-
-      // Show success message
-      this.message = 'L\'article a été ajouté avec succès.';
-    } else {
-      this.message = 'Veuillez remplir tous les champs.';
-    }
-  }
 }
