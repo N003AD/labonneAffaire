efccb558f0f6dd3c5bbe1e5c2c5d927c5187c118
cheikhtidiane diop <cheikhtidiane.diop2@unchk.edu.sn>
2025-01-01 21:48:46 +0100

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SnapFace';
+  secondes: number = 0;
 
+  ngOnInit(){
+    // Création d'un compteur qui se met à jour à chaque seconde (1000 millisecondes)
+    const compteur = interval(1000);
+    compteur.subscribe({
+      next: (v) => this.secondes = v,
+      error: (e) => console.error(e),
+      complete: () => console.info('Compteur terminé')
+    })
+  }
 }
