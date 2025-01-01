3a0b627b50b370cedbb9e0f86e1fd3b7872a705e
cheikhtidiane diop <cheikhtidiane.diop2@unchk.edu.sn>
2025-01-01 15:27:36 +0100

    FormBuilder

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: false,
-  
   templateUrl: './reactive-form.component.html',
-  styleUrl: './reactive-form.component.css'
+  styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-  titleToAdd = new FormControl(' ');
-  priceToAdd = new FormControl(' ');
+  articleForm: FormGroup;
 
-  ngOnInit(): void {
-      
+  constructor() {
+    this.articleForm = new FormGroup({
+      titleToAdd: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      priceToAdd: new FormControl('', [Validators.required, Validators.min(0)]),
+    });
   }
 
-  onSubmit(form: NgForm){
-    const newTitle = this.titleToAdd.value;
-    const newPrice = this.priceToAdd.value;
-    console.log('Le nom du produit est: ' + newTitle);
-    console.log('Le prix du produit est: ' + newPrice);
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.articleForm.valid) {
+      const newTitle = this.articleForm.get('titleToAdd')?.value;
+      const newPrice = this.articleForm.get('priceToAdd')?.value;
+      console.log('Le titre du produit est:', newTitle);
+      console.log('Le prix du produit est:', newPrice);
+    } else {
+      console.error('Le formulaire est invalide. Veuillez remplir tous les champs correctement.');
+    }
   }
 }
-
